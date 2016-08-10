dfe2c9fd23c8ee8ca458b608e737e93eb0f34011
tisong <tianxin675470193@outlook.com>
2016-08-10 16:26:09 +0800

    增加线程分发模块

--- src/java/org/apache/catalina/connector/http/HttpConnector.java
+++ src/java/org/apache/catalina/connector/http/HttpConnector.java
@@ -3,10 +3,14 @@ package org.apache.catalina.connector.http;
 import org.apache.catalina.*;
 import org.apache.catalina.net.DefaultServerSocketFactory;
 import org.apache.catalina.net.ServerSocketFactory;
+import org.apache.catalina.util.LifecycleSupport;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by tisong on 8/9/16.
@@ -23,6 +27,27 @@ public class HttpConnector implements Connector, Lifecycle, Runnable{
 
     private ServerSocketFactory factory;
 
+    private LifecycleSupport lifecycle;
+
+    private List createdProcessors;
+
+    private Stack processors;
+
+    private Thread thread;
+
+    private String threadName;
+
+    private int curProcessors;
+
+    private int minProcessors;
+
+    private int maxProcessors;
+
+
+    private boolean initialized;
+
+    private boolean started;
+
 
 
     // ------------------------------------------------------- implements Connector
@@ -37,7 +62,7 @@ public class HttpConnector implements Connector, Lifecycle, Runnable{
         try {
             serverSocket = open();
         } catch (IOException e) {
-            
+
         }
     }
 
@@ -56,6 +81,20 @@ public class HttpConnector implements Connector, Lifecycle, Runnable{
     @Override
     public void start() throws LifecycleException {
 
+        if (started) {
+            throw new LifecycleException();
+        }
+
+        lifecycle.fireLifecycleEvent(START_EVENT, null);
+
+        this.started = true;
+
+        threadStart();
+
+        while(curProcessors < minProcessors) {
+            HttpProcessor processor = newProcessor();
+            recycleProcessor(processor);
+        }
     }
 
     @Override
@@ -66,6 +105,25 @@ public class HttpConnector implements Connector, Lifecycle, Runnable{
     @Override
     public void run() {
 
+        Socket socket = null;
+
+        try {
+            socket = serverSocket.accept();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        HttpProcessor processor = alloateProcessor();
+        if (processor == null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        processor.process(socket);
     }
 
 
@@ -84,6 +142,7 @@ public class HttpConnector implements Connector, Lifecycle, Runnable{
 
     }
 
+    // -------------------------------------------------------- Private methods
 
     private ServerSocket open() throws IOException {
         ServerSocketFactory factory = getFactory();
@@ -107,4 +166,48 @@ public class HttpConnector implements Connector, Lifecycle, Runnable{
 
         return this.factory;
     }
+
+
+    private void threadStart() {
+
+        thread = new Thread(this, threadName);
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+
+    private HttpProcessor newProcessor() {
+        HttpProcessor processor = new HttpProcessor(this, curProcessors++);
+
+        if (processor instanceof Lifecycle) {
+            try {
+                ((Lifecycle) processor).start();
+            } catch (LifecycleException e) {
+
+                return null;
+            }
+        }
+
+        createdProcessors.add(processor);
+        return processor;
+    }
+
+    private HttpProcessor alloateProcessor() {
+        synchronized (processors) {
+            if (processors.size() > 0) {
+                return (HttpProcessor) processors.pop();
+            } else if (curProcessors >= maxProcessors) {
+                return null;
+            } else {
+                return newProcessor();
+            }
+        }
+    }
+
+    private void recycleProcessor(HttpProcessor processor) {
+
+        processors.push(processor);
+    }
+
+
 }
