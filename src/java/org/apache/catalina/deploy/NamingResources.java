b89fcf5fb81f537e43f2730fcd060e161c388b7b
tisong <tianxin675470193@outlook.com>
2016-08-22 22:45:04 +0800

    资源抽象

---
+++ src/java/org/apache/catalina/deploy/NamingResources.java
@@ -0,0 +1,23 @@
+package org.apache.catalina.deploy;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.util.LifecycleMBeanBase;
+
+import java.io.Serializable;
+
+/**
+ * Created by tisong on 8/21/16.
+ */
+public class NamingResources extends LifecycleMBeanBase implements Serializable{
+
+
+    // ------------------------------------------------------- Lifecycle methods
+
+    @Override
+    protected void initInternal() throws LifecycleException {
+
+        super.initInternal();
+
+
+    }
+}
