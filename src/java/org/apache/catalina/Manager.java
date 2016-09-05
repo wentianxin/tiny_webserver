f64458fc591624d866defb69df85738d02bfa257
tisong <tianxin675470193@outlook.com>
2016-09-05 23:45:12 +0800

    Session属性抽象

--- src/java/org/apache/catalina/Manager.java
+++ src/java/org/apache/catalina/Manager.java
@@ -1,9 +1,36 @@
 package org.apache.catalina;
 
+import java.io.IOException;
+
 /**
  * Created by tisong on 9/5/16.
  */
 public interface Manager {
 
-    
+    public Container getContainer();
+
+    public void setContainer(Container container);
+
+    public int getMaxInactiveInterval();
+
+    public void setMaxInactiveInterval(int interval);
+
+    // --------------------------------------------------------- Public Methods
+
+    public void add(Session session);
+
+    public void remove(Session session);
+
+    public Session createSession();
+
+
+    public Session findSession(String id) throws IOException;
+
+
+    public Session[] findSessions();
+
+
+    public void load() throws IOException;
+
+    public void unload() throws IOException;
 }
--- src/java/org/apache/catalina/Session.java
+++ src/java/org/apache/catalina/Session.java
@@ -1,7 +1,66 @@
 package org.apache.catalina;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by tisong on 9/5/16.
  */
 public interface Session {
+
+    // ------------------------------------------------------------- Properties
+    /**
+     * 创建时间
+     */
+    public long getCreationTime();
+    public void setCreationTime(long time);
+
+    /**
+     * Session Id
+     */
+    public String getId();
+    public void setId(String id);
+
+    /**
+     * 最后访问时期
+     */
+    public long getLastAccessedTime();
+
+    /**
+     * 关联的Manager
+     */
+    public Manager getManager();
+    public void setManager();
+
+    /**
+     * 最大有效时间
+     */
+    public int getMaxInactiveInternal();
+    public void setManInactiveInternal(int internal);
+
+
+    /**
+     * 关联的HttpSession对象
+     */
+    public HttpSession getSession();
+
+    /**
+     * 有效标志位
+     */
+    public void setVaild(boolean isVaild);
+    public boolean isVaild();
+
+
+    // --------------------------------------------------------- Public Methods
+
+    public void access();
+
+
+    public void addSessionListener(SessionListener listener);
+
+    public void removeSessionListener(SessionListener listener);
+
+    public void recycle();
+
+
+
 }
