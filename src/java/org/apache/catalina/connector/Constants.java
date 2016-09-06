e65316792f6aeb0872fbae11c6a5251989849d29
tisong <tianxin675470193@outlook.com>
2016-09-06 13:33:32 +0800

    常量

---
+++ src/java/org/apache/catalina/connector/Constants.java
@@ -0,0 +1,9 @@
+package org.apache.catalina.connector;
+
+/**
+ * Created by tisong on 9/4/16.
+ */
+public final class Constants {
+
+    public static final String Package = "org.apache.catalina.connector";
+}
---
+++ src/java/org/apache/catalina/core/FastEngineMapper.java
@@ -0,0 +1,16 @@
+package org.apache.catalina.core;
+
+import org.apache.catalina.ContainerEvent;
+import org.apache.catalina.ContainerListener;
+
+/**
+ * Created by tisong on 9/5/16.
+ */
+public class FastEngineMapper implements ContainerListener {
+
+
+    @Override
+    public void containerEvent(ContainerEvent event) {
+
+    }
+}
