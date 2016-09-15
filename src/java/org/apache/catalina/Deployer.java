4a7d9f0fed463070f7246f38e21b25a19a3af77f
tisong <tianxin675470193@outlook.com>
2016-09-15 23:24:07 +0800

    接口完善

--- src/java/org/apache/catalina/Deployer.java
+++ src/java/org/apache/catalina/Deployer.java
@@ -1,7 +1,39 @@
 package org.apache.catalina;
 
+import java.io.IOException;
+import java.net.URL;
+
 /**
  * Created by tisong on 9/9/16.
  */
-public class Deployer {
+public interface Deployer {
+
+
+    public String getName();
+
+
+    public void install(String contextPath, URL war);
+
+
+
+    public void install(URL config, URL war) throws IOException;
+
+
+
+    public Context findDepolyedApp(String contextPath);
+
+
+    public String[] findDepolyedApps();
+
+
+
+
+    public void remove(String contextPath) throws IOException;
+
+
+    public void start(String contextPath) throws IOException;
+
+
+
+    public void stop(String contextPath) throws IOException;
 }
