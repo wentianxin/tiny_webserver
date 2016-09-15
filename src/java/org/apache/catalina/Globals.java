330b12caea1a10f5308dc2d78979e47940fa88b9
tisong <tianxin675470193@outlook.com>
2016-09-15 23:24:53 +0800

    常量信息

---
+++ src/java/org/apache/catalina/Globals.java
@@ -0,0 +1,22 @@
+package org.apache.catalina;
+
+/**
+ * Created by tisong on 9/6/16.
+ */
+public final class Globals {
+
+    public static final String WELCOME_FILES_ATTR =
+            "org.apache.catalina.WELCOME_FILES";
+
+
+    public static final String WORK_DIR_ATTR =
+            "javax.servlet.context.tempdir";
+
+
+
+    public static final String SESSION_PARAMETER_NAME = "jsessionid";
+
+
+    public static final String RESOURCES_ATTR =
+            "org.apache.catalina.resources";
+}
--- src/java/org/apache/catalina/startup/Constants.java
+++ src/java/org/apache/catalina/startup/Constants.java
@@ -9,6 +9,20 @@ public final class Constants {
 
     public static final String DefaultWebXml = "conf/web.xml";
 
+    public static final String WebDtdResourcePath_22 =
+            //      "conf/web_22.dtd";
+            "/javax/servlet/resources/web-app_2_2.dtd";
+
+    public static final String WebDtdResourcePath_23 =
+            //      "conf/web_23.dtd";
+            "/javax/servlet/resources/web-app_2_3.dtd";
+
+
+    public static final String WebDtdPublicId_22 =
+            "-//Sun Microsystems, Inc.//DTD Web Application 2.2//EN";
+
+    public static final String WebDtdPublicId_23 =
+            "-//Sun Microsystems, Inc.//DTD Web Application 2.3//EN";
 
 
 }
